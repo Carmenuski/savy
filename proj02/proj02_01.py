8d3bb10f8e65f27f95f1d849f26a56e0dba530b6
Carmenuski <Carmenuski@users.noreply.github.com>
2017-07-11 11:24:30 -0500

    Add files via upload

--- proj02/proj02_01.py
+++ proj02/proj02_01.py
@@ -1,15 +1,12 @@
-# Name:
-# Date:
-
+# Name: Nunya buisness
+# Date: Dunno
 # proj02: sum
-
 # Write a program that prompts the user to enter numbers, one per line,
 # ending with a line containing 0, and keep a running sum of the numbers.
 # Only print out the sum after all the numbers are entered
 # (at least in your final version). Each time you read in a number,
 # you can immediately use it for your sum,
 # and then be done with the number just entered.
-
 #Example:
 # Enter a number to sum, or 0 to indicate you are finished: 4
 # Enter a number to sum, or 0 to indicate you are finished: 5
@@ -17,3 +14,18 @@
 # Enter a number to sum, or 0 to indicate you are finished: 10
 # Enter a number to sum, or 0 to indicate you are finished: 0
 #The sum of your numbers is: 21
+("addition")
+number1 = raw_input("Enter a number: ")
+print number1
+number2 = raw_input("Enter a number: ")
+print number2
+number3 = raw_input("Enter a number: ")
+print number3
+number4 = raw_input("Enter a number: ")
+print number4
+A = int(number1)
+B = int(number2)
+C = int(number3)
+D = int(number4)
+E = A+B+C+D
+print E, "is the answer."
