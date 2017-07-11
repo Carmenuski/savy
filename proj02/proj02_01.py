be4f154e61df41c49d8fca4ccca6a80d786a0f4d
Carmenuski <Carmenuski@users.noreply.github.com>
2017-07-11 14:40:17 -0500

    Add files via upload

--- proj02/proj02_01.py
+++ proj02/proj02_01.py
@@ -1,5 +1,5 @@
-# Name: Nunya buisness
-# Date: Dunno
+# Name:
+# Date:
 # proj02: sum
 # Write a program that prompts the user to enter numbers, one per line,
 # ending with a line containing 0, and keep a running sum of the numbers.
@@ -13,19 +13,17 @@
 # Enter a number to sum, or 0 to indicate you are finished: 2
 # Enter a number to sum, or 0 to indicate you are finished: 10
 # Enter a number to sum, or 0 to indicate you are finished: 0
-#The sum of your numbers is: 21
-("addition")
-number1 = raw_input("Enter a number: ")
-print number1
-number2 = raw_input("Enter a number: ")
-print number2
-number3 = raw_input("Enter a number: ")
-print number3
-number4 = raw_input("Enter a number: ")
-print number4
-A = int(number1)
-B = int(number2)
-C = int(number3)
-D = int(number4)
-E = A+B+C+D
-print E, "is the answer."
+# The sum of your number is 21
+sum = 0
+loop_control = True
+while loop_control==True:
+    number1 = int(raw_input("Enter a number to add, or 0 to show that you are finished: "))
+    #sum = number1
+    if int(number1)!=0:
+
+        sum = sum + number1
+        number1=("Enter a number to add, or 0 to show that you are finished: ")
+    else:
+        loop_control = False
+
+print sum, "is the sum of your numbers"
